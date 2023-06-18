9293cc97e17093f4ce7be2f4f67624437a2733cd
brykalina yulia <arik16@yandex.ru>
2023-06-18 23:22:58 +0300

    ver 1

--- src/main/java/com/example/demovacjava/controllers/ResultController.java
+++ src/main/java/com/example/demovacjava/controllers/ResultController.java
@@ -1,15 +1,19 @@
 package com.example.demovacjava.controllers;
 
 import com.example.demovacjava.models.Input;
+import com.example.demovacjava.models.OutputDb;
 import com.example.demovacjava.repositories.OutputDBRepository;
 import com.example.demovacjava.services.Output;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@Controller
 public class ResultController {
     private final Output output;
+    @Autowired
+    private OutputDBRepository repository;
 
     public ResultController(Output output) {
         this.output = output;
@@ -19,6 +23,8 @@ public class ResultController {
         Input input = new Input(day, month, year);
         var result = output.result(input);
         model.addAttribute("result", result);
+        OutputDb db = new OutputDb(result);
+        repository.save(db);
         return "/result";
     }
 }
